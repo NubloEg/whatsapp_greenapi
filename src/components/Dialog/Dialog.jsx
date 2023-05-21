2bc2c3b30c27b25144c5b6ad0f5ec92e5f784ded
NubloEg <NubloEg@github.com>
2023-05-21 10:51:37 +0300

    new change

--- src/components/Dialog/Dialog.jsx
+++ src/components/Dialog/Dialog.jsx
@@ -9,10 +9,10 @@ export default function Dialog({setFriend,user,idInstLogin,ApiTokenLogin}) {
 React.useEffect(()=>{
   setInterval(()=>{
     getMessages(idInstLogin,ApiTokenLogin).then(resp=>{
-      console.log(resp.data)
+      console.log(resp.data.body)
     })
   },5000)
-},[])
+},[idInstLogin,ApiTokenLogin])
 
   const sendMessage=()=>{
     if (message) {
